1b1ceb1c61776d9ea0e36e3a16cbb6f92c6aca72
AreejRamzi <areej.ramsi@gmail.com>
2019-03-13 14:13:49 +0300

    copy

--- game.js
+++ game.js
@@ -3,199 +3,276 @@
 var turn = "x";
 var playerX = 0;
 var playerO = 0;
-var gameInPlay = false
+var gameInPlay = false;
+
 
 $(".box").on("click", function (event) {
-console.log($(event.target).css("background-image","none"));
-console.log(gameInPlay);
+    console.log($(event.target).css("background-image", "none"));
+    console.log(gameInPlay);
 
-    if ($(event.target).css("background-image","none")){
+    if ($(event.target).css("background-image", "none")) {
 
-    if(!gameInPlay){
-        if (turn == "x") {
-            $(event.target).css({
-                "background-image": "url('./Ribo.png')",
-                "background-size": "157px 147px"
-            });
-            $(event.target).addClass("x");
-            // $(event.target).off("click");
-            turn = "o";
-    
+        if (!gameInPlay) {
+            if (turn == "x") {
+                $(event.target).css({
+                    "background-image": "url('./Ribo.png')",
+                    "background-size": "157px 147px"
+                });
+                
+                $(event.target).addClass("x");
+                // $(event.target).off("click");
+                turn = "o";
+
+            }
+            else {
+                $(event.target).css({
+                    "background-image": "url('./Jass.png')",
+                    "background-size": "157px 147px"
+                });
+               
+                // $(event.target).off("click");
+                $(event.target).addClass("o");
+                turn = "x";
+            }
         }
-        else {
-            $(event.target).css({
-                "background-image": "url('./Jass.png')",
-                "background-size": "157px 147px"
-            });
-            // $(event.target).off("click");
-            $(event.target).addClass("o");
-            turn = "x";
-        }
+    };
+
+    if (($(".num1").hasClass("x")) && ($(".num2").hasClass("x")) && ($(".num3").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "rock!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX)
+        $('#playerA').text(playerX);
+        gameInPlay = true
+        reset()
+
     }
-};
+    if (($(".num4").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num6").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "rock!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num7").hasClass("x")) && ($(".num8").hasClass("x")) && ($(".num9").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "way to go!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num1").hasClass("x")) && ($(".num4").hasClass("x")) && ($(".num7").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "Awesome!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num2").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num8").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "star of the show!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num3").hasClass("x")) && ($(".num6").hasClass("x")) && ($(".num9").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "genius!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num1").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num9").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "Tony Stark!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num3").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num7").hasClass("x"))) {
+        console.log("x winner");
+        playerX++;
+        swal({
+            title: "rock!",
+            icon: "images/areebwin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        $('#playerA').text(playerX);
+        //$('#playerJ').text("0");
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num1").hasClass("o")) && ($(".num2").hasClass("o")) && ($(".num3").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "sweet like a flower!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        // $('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+        reset()
+    }
+    if (($(".num4").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num6").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "tough like a diamond!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        // $('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+     
+        reset()
+    }
+    if (($(".num7").hasClass("o")) && ($(".num8").hasClass("o")) && ($(".num9").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "smarty pants!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        //$('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+    
+        reset()
+    }
+    if (($(".num1").hasClass("o")) && ($(".num4").hasClass("o")) && ($(".num7").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "勝者!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        //$('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+    
+        reset()
+    }
+    if (($(".num2").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num8").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "rock star!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        //$('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+  
+        reset()
+    }
+    if (($(".num3").hasClass("o")) && ($(".num6").hasClass("o")) && ($(".num9").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "pumpkin!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        //$('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+  
+        reset()
+    }
+    if (($(".num1").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num9").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "sweet like a flower tough like a diamond!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        //$('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+   
+        reset()
+    }
+    if (($(".num3").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num7").hasClass("o"))) {
+        console.log("o winner");
+        playerO++;
+        swal({
+            title: "sweet like a flower tough like a diamond!",
+            icon: "images/jasminewin.png",
+        });
+        console.log(playerO)
+        console.log(playerX);
+        // $('#playerJ').text(playerO);
+        $('#playerJ').text(playerO);
+        gameInPlay = true
+       
+        reset()
+    }
 
-if (($(".num1").hasClass("x")) && ($(".num2").hasClass("x")) && ($(".num3").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX)
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
+   
+    });
 
-}
-if (($(".num4").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num6").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num7").hasClass("x")) && ($(".num8").hasClass("x")) && ($(".num9").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num1").hasClass("x")) && ($(".num4").hasClass("x")) && ($(".num7").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num2").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num8").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num3").hasClass("x")) && ($(".num6").hasClass("x")) && ($(".num9").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num1").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num9").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num3").hasClass("x")) && ($(".num5").hasClass("x")) && ($(".num7").hasClass("x"))) {
-    console.log("x winner");
-    playerX++;
-    console.log(playerO)
-    console.log(playerX);
-    $('#playerA').text(playerX);
-    //$('#playerJ').text("0");
-    gameInPlay = true
-    reset()
-}
-if (($(".num1").hasClass("o")) && ($(".num2").hasClass("o")) && ($(".num3").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-   // $('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num4").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num6").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-   // $('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num7").hasClass("o")) && ($(".num8").hasClass("o")) && ($(".num9").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    //$('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num1").hasClass("o")) && ($(".num4").hasClass("o")) && ($(".num7").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    //$('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num2").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num8").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    //$('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num3").hasClass("o")) && ($(".num6").hasClass("o")) && ($(".num9").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    //$('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num1").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num9").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    //$('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-if (($(".num3").hasClass("o")) && ($(".num5").hasClass("o")) && ($(".num7").hasClass("o"))) {
-    console.log("o winner");
-    playerO++;
-    console.log(playerO)
-    console.log(playerX);
-    // $('#playerJ').text(playerO);
-    $('#playerJ').text(playerO);
-    gameInPlay = true
-    reset()
-}
-});
+
 
 
 $("#reset").on("click", function (event) {
@@ -203,13 +280,11 @@ $("#reset").on("click", function (event) {
 });
 
 
-function reset(){
-    // $('.box').removeAttr("style")
+function reset() {
     $('.box').removeClass("x")
     $('.box').removeClass("o")
 }
 $("#playAgin").on("click", function (event) {
-    // $(".box").css("background-image", "none");
     $('.box').removeAttr("style")
     $('.box').removeClass("x")
     $('.box').removeClass("o")
